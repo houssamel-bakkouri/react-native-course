03902804538130aa931f9b591b9ae2c0849d73db
houssamElbakkouri <89257103+houssamElbakkouri@users.noreply.github.com>
2024-01-14 18:41:36 +0100

    Initial commit (for real this time)

---
+++ RNCourse/App.js
@@ -0,0 +1,42 @@
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+
+export default function App() {
+    return (
+        <View style={styles.appContainer}>
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.testInput} placeholder={'Your course goal !'}/>
+                <Button title={"Add Goal"}/>
+            </View>
+            <View style={styles.goalsContainer}>
+                <Text>List of goals...</Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    appContainer: {
+        flex : 1,
+        paddingTop: 50,
+        paddingHorizontal : 16
+    },
+    inputContainer: {
+        flex : 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems : "center",
+        marginBottom : 24,
+        borderBottomWidth: 1,
+        borderBottomColor : '#cccccc'
+    },
+    testInput: {
+        borderWidth: 1,
+        borderColor: '#cccccc',
+        width: '70%',
+        marginRight: 8,
+        padding: 8
+    },
+    goalsContainer : {
+        flex : 5
+    }
+});
